fa1e2a72ebbf075b5f39c1dfcb2ab37be91fa160
tsh235 <tanyashustrova@yandex.ru>
2024-01-04 17:13:45 +0300

    ДЗ модуль 3 урок 3 задание 1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 import { timer } from "./timer.js";
 
 timer();
+
+const items = document.querySelectorAll('.travel__item');
+const buttons = document.querySelectorAll('.travel__item-title');
+const textWrappers = document.querySelectorAll('.travel__item-text-wrapper');
+
+let heightWrapper = 0;
+
+textWrappers.forEach(elem => {
+  if (heightWrapper < elem.scrollHeight) {
+    heightWrapper = elem.scrollHeight;
+  }
+});
+
+buttons.forEach((btn, index) => {
+
+  btn.addEventListener('click', () => {
+
+    for (let i = 0; i < items.length; i++) {
+      if (index === i) {
+        textWrappers[i].style.height =
+          items[i].classList.contains('travel__item_active') ?
+          '' : `${heightWrapper}px`;
+          items[i].classList.toggle('travel__item_active');
+      } else {
+        textWrappers[i].style.height = '';
+        items[i].classList.remove('travel__item_active');
+      }
+    }
+
+  });
+
+});
