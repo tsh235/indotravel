fff8aa42d30272ce519d7655ad0b0851a7e194c3
tsh235 <tanyashustrova@yandex.ru>
2024-01-06 20:15:03 +0300

    правка аккордеона

--- js/main.js
+++ js/main.js
@@ -14,20 +14,33 @@ textWrappers.forEach(elem => {
 });
 
 items.forEach(item => {
-  item.addEventListener('click', ({target}) => {
+  item.addEventListener('click', () => {
     const textWrapper = item.querySelector('.travel__item-text-wrapper');
 
     if (textWrapper.style.height === '') {
+      items.forEach(i => {
+        i.classList.remove('travel__item_active');
+        textWrappers.forEach(tw => {
+          tw.style.height = '';
+        })
+      });
       textWrapper.style.height =
         item.classList.contains('travel__item_active') ?
         '' : `${heightWrapper}px`;
-      item.classList.toggle('travel__item_active');
+      item.classList.add('travel__item_active');
     } else {
       textWrapper.style.height = '';
       item.classList.remove('travel__item_active');
     }
   })
 });
+
+document.querySelectorAll('li').forEach(item => {
+  item.addEventListener('click', () => {
+    document.querySelectorAll('li').forEach(li => li.classList.remove('active'));
+    item.classList.add('active');
+  })
+})
 
 // buttons.forEach((btn, index) => {
 //   btn.addEventListener('click', () => {
